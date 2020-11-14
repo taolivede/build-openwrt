9a7e5f7074a419215cb0ffa9992ec448712ef221
taolivede <tao.li@live.de>
2020-11-14 22:37:45 +0100

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -3,7 +3,7 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.178.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.178.3/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' package/lean/default-settings/files/zzz-default-settings
@@ -12,7 +12,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' packa
 sed -i "s/OpenWrt /OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
@@ -40,4 +40,4 @@ sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/
 sed -i 's/"Web 管理"/"Web"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
 sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
-sed -i 's/"Argon 主题设置"/"Argon设置"/g' package/lean/luci-app-argon-config/po/zh-cn/argon-config.po
+# sed -i 's/"Argon 主题设置"/"Argon设置"/g' package/lean/luci-app-argon-config/po/zh-cn/argon-config.po
